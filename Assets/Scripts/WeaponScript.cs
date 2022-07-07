ec67fa859a083a150ced59f59ae0931d61ec4b8f
Logan Groves <logangrovesDev@gmail.com>
2022-07-07 15:10:18 -0700

    Basic Weapon Functionality
    Implemented gun and basics, added walls to environment.

---
+++ Assets/Scripts/WeaponScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour
+{
+    public Transform bulletSpawn;
+    public GameObject bulletPrefab;
+
+    public float bulletVelocity = 30f;
+    public float fireRate = 0.2f;
+
+    private bool canShoot = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && canShoot)
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        StartCoroutine(FireDelay());
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+        bullet.GetComponent<Rigidbody>().velocity = transform.TransformDirection(Vector3.forward * bulletVelocity);
+    }
+
+    IEnumerator FireDelay()
+    {
+        Debug.Log("here: " + canShoot);
+        canShoot = false;
+        yield return new WaitForSeconds(fireRate);
+        canShoot = true;
+    }
+}
